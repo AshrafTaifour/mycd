5f36edf6c0d3782e80255da8bbf95167f657b27f
Ashraf Taifour <ashraftayfour@gmail.com>
2021-05-28 16:28:10 -0400

    testing java.io.file as a way to check directories

--- Mycd.java
+++ Mycd.java
@@ -1,7 +1,27 @@
+import java.io.File;
 public class Mycd {
+	public static String changeDir(String src, String dest){
+		StringBuilder result = new StringBuilder();
+
+
+
+		return result.toString();
+		
+	}
+
+
 	public static void main(String[]args){
-		System.out.println("test");
-	
+		
+		String test = "/abc/def/ghi";
+		
+		String dest = "/";
+
+		File resultDirectory = new File(dest);
+
+		File testDir = new File(test);
+
+		System.out.println(resultDirectory.exists());
+		System.out.println(testDir.exists());
 	}
 
 }
